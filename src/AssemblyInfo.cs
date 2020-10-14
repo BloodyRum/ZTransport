b852b3c8accda8c0d9dd3b559b2c75143eaded71
BloodyRum <bloodyrum@tejat.net>
2020-10-14 17:05:49 -0400

    Changed version to todays date, 10-14-2020

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.1.25.0")]
-[assembly: AssemblyFileVersion("2020.1.25.0")]
+[assembly: AssemblyVersion("2020.10.14.0")]
+[assembly: AssemblyFileVersion("2020.10.14.0")]
